ce2cd016b89ab0f0c317f26a999c01325efabaa7
LosDevils <108871397+LosDevils@users.noreply.github.com>
2023-11-17 13:53:06 +0300

    Add Menu Handlers or Games Keyboards/Buttons.

--- src/api/handlers/menu.py
+++ src/api/handlers/menu.py
@@ -8,3 +8,28 @@ router = Router()
 @router.message(F.text == buttons.menu.profile)
 async def profile(message: Message):
     pass
+
+
+@router.message(F.text == buttons.menu.games)
+async def games(message: Message):
+    pass
+
+
+@router.message(F.text == buttons.menu.setting)
+async def setting(message: Message):
+    pass
+
+
+@router.message(F.text == buttons.menu.support)
+async def support(message: Message):
+    pass
+
+
+@router.message(F.text == buttons.menu.profile)
+async def profile(message: Message):
+    pass
+
+
+@router.message(F.text == buttons.menu.profile)
+async def profile(message: Message):
+    pass
